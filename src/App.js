b5bc5aee163d6c47fef95d34002806c046087ee4
lasker61 <laskernahid2@gmail.com>
2021-06-14 20:46:12 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './App.css';
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+
+function App() {
+  return (
+    <nav className='App'>
+         <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    </nav>
+  );
+}
+
+export default App;
